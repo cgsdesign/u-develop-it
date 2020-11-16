cab351a77498e15233b6424a0685310c26db6ad0
Cat <CGraham@CatherinesMBP2.attlocal.net>
2020-11-16 17:46:54 -0600

    notes started

---
+++ LessonSQL.js
@@ -0,0 +1,3 @@
+//start mySQL shell: mysql -u root -p
+//will have to enter password (see notes)
+//if get coand not found try: export PATH="${PATH}:/usr/local/mysql/bin/" to .bash_profile or .zshrc (will have to restart terminal after for this line to take effect)
